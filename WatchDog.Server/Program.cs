95f917b9f0f0d97a1464fc5dd14b86df430e3ef5
Othon Valentim <othonvvalentim@gmail.com>
2025-04-04 21:33:13 -0300

    Server

--- WatchDog.Server/Program.cs
+++ WatchDog.Server/Program.cs
@@ -1,25 +1,112 @@
+using System.Net.WebSockets;
+using System.Net;
+using System.Text;
+
+Console.WriteLine("\nWebSocket server starting...\n");
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls(builder.Configuration["Host"]!);
 
 var app = builder.Build();
+app.UseWebSockets();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var connections = new List<WebSocket>();
+
+app.Map("/ws", async context =>
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	if (context.WebSockets.IsWebSocketRequest)
+	{
+		var curName = context.Request.Query["name"];
+		var accessKey = context.Request.Query["accessKey"];
+
+		var serverAccessKey = builder.Configuration["AccessKey"];
+		if (accessKey != serverAccessKey)
+		{
+			return;
+		}
+
+		using var ws = await context.WebSockets.AcceptWebSocketAsync();
+		connections.Add(ws);
+
+		await Broadcast($"{curName} joined the room");
+		await Broadcast($"{connections.Count} users connected");
+
+		await ReceiveMessage(ws,
+			async (result, buffer) =>
+			{
+				if (result.MessageType is WebSocketMessageType.Text)
+				{
+					try
+					{
+						var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+						await Broadcast(curName + ": " + message);
+					}
+					catch (Exception ex)
+					{
+						await Broadcast($"{curName} message error: {ex.Message}");
+						Console.WriteLine(ex);
+					}
+				}
+				else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+				{
+					connections.Remove(ws);
+					await Broadcast($"{curName} left the room");
+					await Broadcast($"{connections.Count} users connected");
+					await ws.CloseAsync(result.CloseStatus!.Value, result.CloseStatusDescription, CancellationToken.None);
+				}
+			});
+	}
+	else
+	{
+		context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+	}
+});
+
+async Task ReceiveMessage(WebSocket webSocket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+{
+	var buffer = new byte[1024 * 4];
+	while (webSocket.State is WebSocketState.Open)
+	{
+		try
+		{
+			var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+			handleMessage(result, buffer);
+		}
+		catch
+		{
+			RemoveConnection(webSocket);
+		}
+	}
 }
 
-app.UseHttpsRedirection();
+async Task Broadcast(string message)
+{
+	var bytes = Encoding.UTF8.GetBytes(message);
+	lock (connections)
+	{
+		connections = [.. connections.Where(socket => socket.State == WebSocketState.Open)];
+	}
 
-app.UseAuthorization();
+	foreach (var socket in connections.ToList())
+	{
+		try
+		{
+			var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+			await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+		}
+		catch
+		{
+			RemoveConnection(socket);
+		}
+	}
+}
 
-app.MapControllers();
+void RemoveConnection(WebSocket ws)
+{
+	lock (connections)
+	{
+		connections.Remove(ws);
+	}
+}
 
-app.Run();
+await app.RunAsync();
